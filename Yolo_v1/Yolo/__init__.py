b9f443ad1071ed06586071c63d541ea5bcefdb0c
Caroon Wang <caroonwang@qq.com>
2020-07-22 16:44:42 +0800

    Test Load Weights

--- Yolo_v1/Yolo/__init__.py
+++ Yolo_v1/Yolo/__init__.py
@@ -1,2 +1,3 @@
 from .darknet import DarkNet
 from .lenet import LeNet
+from .googlenet import GoogLeNet
--- Yolo_v1/Yolo_v1.py
+++ Yolo_v1/Yolo_v1.py
@@ -1,60 +1,140 @@
+import sys
+import struct
 import torch
-import torch.optim as optim
-import torchvision
-import torchvision.transforms as transforms
-from Yolo import loss
 from Yolo import DarkNet
 from Yolo import LeNet
-from Yolo import dataloader
-import matplotlib.pyplot as plt
-import numpy as np
-import math
+from Yolo import GoogLeNet
 
-#The path and parameters needed to be specified depending on the device
-#where to download the dataset
-data_download_path = 'C:\\Users\\ASUS\\Desktop\\temp_data\\data\\'
-#where to save the model
-PATH = '.\\voc_net.pth'
-#batch size
-b_size = 16
+#class network:
+#    def __init__(self):
+#        self.n = 0
+#        self.batch = 0
+#        self.seen = 0
 
-transform = transforms.Compose([
-    transforms.Resize((448, 448)),
-    transforms.ToTensor(),
-    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-trainset = torchvision.datasets.VOCDetection(root = data_download_path, year = '2012', image_set = 'train',
-                                             download = True, transform = transform)
+def load_weights_upto(net, filename):
+    s_int = 4
+    s_float = 4
 
-trainsetloader = dataloader.VOC_DataLoader(trainset, batch_size = b_size)
+    print('Loading weights from {}'.format(filename))
 
-net = DarkNet()
-optimizer = optim.SGD(net.parameters(), lr = 0.001, momentum = 0.9)
+    fp = open(filename, "rb")
+    if fp == False: 
+        print('Cannot open the file')
 
-for epoch in range(2):
-    running_loss = 0.0
-    images = trainsetloader[0]
-    annotations = trainsetloader[1]
+    major = struct.unpack('i', fp.read(s_int))[0]
+    minor = struct.unpack('i', fp.read(s_int))[0]
+    revision = struct.unpack('i', fp.read(s_int))[0]
+    
+#    iseen = fp.read(4)
+#    iseen = struct.unpack('i', fp.read(s_int))[0]
 
-    for i in range(len(images)):
-        inputs, labels = images[i], annotations[i]
+    if ((major*10 + minor) >= 2 and major < 1000 and minor < 1000):
+        iseen = struct.unpack('i', fp.read(s_int))[0]
+        #fread(net->seen, sizeof(size_t), 1, fp)
+    else:
+        iseen = 0;
+        iseen = struct.unpack('i', fp.read(s_int))[0]
+#        fread(&iseen, sizeof(int), 1, fp);
+#        *net->seen = iseen;
+    print('Major: {}\nMinor: {}\nRevision: {}\niseen: {}\n'.format(major, minor, revision, iseen))
 
-        print('\nBatch No: {}'.format(i+1))
-#        print(labels)
-        optimizer.zero_grad()
+    transpose = True if ((major > 1000) or (minor > 1000)) else False
 
-        outputs = net(inputs)
-        loss_value = loss.yolo_loss(outputs, labels)
-        loss_value.backward()
-        optimizer.step()
+    test_dict = net.state_dict()
+    
+#    print(net.state_dict())
 
-        running_loss += loss_value.item()
-#        print('running loss: {}'.format(loss_value.item()))
+    for key in test_dict:
+        t_l_weight = []
 
-        if i % 10 == 9:
-            print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / 10))
-            running_loss = 0.0
+        if key[0:2] == 'co' and key[-4:] == 'bias':
+            print('Processing convolutional layer\'s bias: ')
+            t_c_b = test_dict[key].shape[0]
+            print('Bias Size: {}'.format(t_c_b))
+            for j in range(t_c_b):
+                t_w = struct.unpack('f', fp.read(s_float))[0]
+                t_l_weight.append(t_w)
 
-print('Finished Training')
-torch.save(net.state_dict(), PATH)
+            print(test_dict[key])
+            print(t_l_weight)
+
+            test_dict[key] = torch.tensor(t_l_weight, dtype = torch.float32)
+            print('Number of Bias loaded: {}'.format(test_dict[key].size()))
+
+            print(test_dict[key])
+
+        if key[0:2] == 'co' and key[-4:] == 'ight':
+            print('Processing convolutional layer\'s weights: ')
+            t_c_out = test_dict[key].shape[0]
+            t_c_in = test_dict[key].shape[1]
+            t_c_k = test_dict[key].shape[2]
+
+            print('Output Channel: {}\nInput Channel: {}\nKernel Size: {}\n'.format(t_c_out, t_c_in, t_c_k))
+
+        if key[0:2] == 'fc' and key[-4:] == 'bias':
+            print('Processing fully connected layer\'s bias: ')
+            t_f_b = test_dict[key].shape[0]
+            print('Bias Size: {}\n'.format(t_f_b))
+
+
+        if key[0:2] == 'fc' and key[-4:] == 'ight':
+            print('Processing fully connected layer\'s weights: ')
+            t_f_out = test_dict[key].shape[0]
+            t_f_in = test_dict[key].shape[1]
+            print('Output Size: {}\nInput Size: {}\n'.format(t_f_out, t_f_in))
+
+    print('Done!\n');
+    fp.close()
+
+
+filename = 'C:\\Users\\ASUS\\Desktop\\temp_data\\data\\Yolo_v1\\extraction.conv.weights'
+
+#net = network()
+net = GoogLeNet()
+net_2 = LeNet()
+
+load_weights_upto(net, filename)
+
+#print(net_2.parameters)
+params = list(net_2.parameters())
+print('The number of parameters in DarkNet is: {}'.format(len(params)))
+
+#print(net_2.state_dict())
+test_dict = net_2.state_dict()
+
+#print(test_dict['conv1.weight'])
+#print(test_dict['conv1.weight'].size())
+#print(test_dict['conv1.weight'].shape[0])
+#print(len(test_dict))
+
+for key in test_dict:
+#    print(key[0:2])
+#    print(key[-4:])
+    if key[0:2] == 'co' and key[-4:] == 'ight':
+        print('Processing convolutional layer\'s weights: ')
+        t_c_out = test_dict[key].shape[0]
+        t_c_in = test_dict[key].shape[1]
+        t_c_k = test_dict[key].shape[2]
+        print('Output Channel: {}\nInput Channel: {}\nKernel Size: {}\n'.format(t_c_out, t_c_in, t_c_k))
+
+    if key[0:2] == 'co' and key[-4:] == 'bias':
+        print('Processing convolutional layer\'s bias: ')
+        t_c_b = test_dict[key].shape[0]
+        print('Bias Size: {}\n'.format(t_c_b))
+
+    if key[0:2] == 'fc' and key[-4:] == 'ight':
+        print('Processing fully connected layer\'s weights: ')
+        t_f_out = test_dict[key].shape[0]
+        t_f_in = test_dict[key].shape[1]
+        print('Output Size: {}\nInput Size: {}\n'.format(t_f_out, t_f_in))
+
+    if key[0:2] == 'fc' and key[-4:] == 'bias':
+        print('Processing fully connected layer\'s bias: ')
+        t_f_b = test_dict[key].shape[0]
+        print('Bias Size: {}\n'.format(t_f_b))
+
+
+
+
+
